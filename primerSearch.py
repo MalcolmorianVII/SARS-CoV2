bcc8040d75f43dac067bf61e07fc2fb76ae6ae53
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2021-08-04 15:28:58 +0200

    Improved readability

--- primerSearch.py
+++ primerSearch.py
@@ -9,16 +9,20 @@ ref_file = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\SARS-CoV-2.reference.fas
 ref_seq = SeqIO.read(ref_file,"fasta").seq
 
 
-df = pd.read_excel(primers_file,engine='openpyxl')
-df['Start'] = ""
-df['End']   = ""
-for index,row in df.iterrows():
+primer_df = pd.read_excel(primers_file,engine='openpyxl') # Representing the primers file as pandas dataframe
+
+#Adding two columns ,Start & End to the primer_df
+primer_df['Start'] = ""
+primer_df['End']   = ""
+
+# Iterating over the primer_df and finding the Start & End pos of the primers
+for index,row in primer_df.iterrows():
     primer = row['Sequence']
     pos = ref_seq.find(primer)
     if pos == -1:   # Reverse primer
         reversed = Seq(primer).reverse_complement()
         pos = ref_seq.find(reversed)
-    df['Start'][index] = pos
-    df['End'][index] = len(primer) + pos
-print(df)
+    primer_df['Start'][index] = pos
+    primer_df['End'][index] = len(primer) + pos
+print(primer_df)
 
