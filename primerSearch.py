506d1cb653409c32a2ef259a30afeb378f3cf98b
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2021-08-04 15:21:29 +0200

    Added the End column

--- primerSearch.py
+++ primerSearch.py
@@ -2,22 +2,23 @@ from Bio.Seq import Seq
 from Bio import SeqIO
 import pandas as pd
 
-primers = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\Midnight_SARS_CoV2_sequences_only.xlsx'
+primers_file = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\Midnight_SARS_CoV2_sequences_only.xlsx'
 
-ref = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\SARS-CoV2-2.reference.fasta'
+ref_file = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\SARS-CoV-2.reference.fasta'
 
-ref_seq = SeqIO.read(ref,"fasta").seq
+ref_seq = SeqIO.read(ref_file,"fasta").seq
 
-# print(ref_seq.find('ACCAACCAACTTTCGATCTCTTGT'))
 
-df = pd.read_excel(primers,engine='openpyxl')
-df['PrimerPos'] = ""
+df = pd.read_excel(primers_file,engine='openpyxl')
+df['Start'] = ""
+df['End']   = ""
 for index,row in df.iterrows():
-    # print(ref_seq.find(row['Sequence']))
-    pos = ref_seq.find(row['Sequence'])
-    if pos == -1:
-        reversed = Seq(row['Sequence']).reverse_complement()
+    primer = row['Sequence']
+    pos = ref_seq.find(primer)
+    if pos == -1:   # Reverse primer
+        reversed = Seq(primer).reverse_complement()
         pos = ref_seq.find(reversed)
-    df['PrimerPos'][index] = pos
+    df['Start'][index] = pos
+    df['End'][index] = len(primer) + pos
 print(df)
 
