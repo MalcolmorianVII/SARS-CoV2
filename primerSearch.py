92fa05568e80c7d0798eb831add16075b1a4163f
MalcolmorianVII <bdkutambe@gmail.com>
2021-08-04 21:18:36 +0200

    Renaming for readability

--- primerSearch.py
+++ primerSearch.py
@@ -6,23 +6,21 @@ primers_file = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\Midnight_SARS_CoV2_s
 
 ref_file = 'C:\\Users\\Belson\\Documents\\SNR-SARS-Cov\\SARS-CoV-2.reference.fasta'
 
-ref_seq = SeqIO.read(ref_file,"fasta").seq
+ref_seq = SeqIO.read(ref_file, "fasta").seq
 
+primers_df = pd.read_excel(primers_file, engine='openpyxl')  # Representing the primers file as pandas dataframe
 
-primer_df = pd.read_excel(primers_file,engine='openpyxl') # Representing the primers file as pandas dataframe
+# Adding two columns ,Start & End to the primers_df
+primers_df['Start'] = ""
+primers_df['End'] = ""
 
-#Adding two columns ,Start & End to the primer_df
-primer_df['Start'] = ""
-primer_df['End']   = ""
-
-# Iterating over the primer_df and finding the Start & End pos of the primers
-for index,row in primer_df.iterrows():
-    primer = row['Sequence']
-    pos = ref_seq.find(primer)
-    if pos == -1:   # Reverse primer
-        reversed = Seq(primer).reverse_complement()
-        pos = ref_seq.find(reversed)
-    primer_df['Start'][index] = pos
-    primer_df['End'][index] = len(primer) + pos
-print(primer_df)
-
+# Iterating over the primers_df and finding the Start & End pos of the primers
+for index, row in primers_df.iterrows():
+    primer_seq = row['Sequence']
+    pos = ref_seq.find(primer_seq)
+    if pos == -1:  # That means a reverse primer
+        rev_comp = Seq(primer_seq).reverse_complement()
+        pos = ref_seq.find(rev_comp)
+    primers_df['Start'][index] = pos
+    primers_df['End'][index] = len(primer_seq) + pos
+print(primers_df)
